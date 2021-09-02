2029fd48db07b6f2cb59bd718ec2f173d70a81d0
Scott Donnan <scott.donnan@me.com>
2021-09-02 17:02:39 -0500

    commit tree 43b1f08edd1cd040e279f530182c7d4054c88382

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-main.remove();
+document.querySelector('main').remove();
 
 const newHeader = document.createElement('h1');
-document.body.appendChild(newHeader);
 newHeader.id = 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.textContent = 'is the champion';
+
